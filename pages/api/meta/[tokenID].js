19fa3e8cc57023ac8fc2d743835bab0add33f989
v1xingyue <qixingyue@126.com>
2022-11-24 16:47:09 +0800

    url 命名参数

--- pages/api/meta/[tokenID].js
+++ pages/api/meta/[tokenID].js
@@ -1,8 +1,10 @@
 const handler = async (req, res) => {
+    const { tokenID } = req.query
     res.json({
         "name": "hello",
         "description": "hello world",
         "image": "https://ipfs.io/ipfs/bafkreifvhjdf6ve4jfv6qytqtux5nd4nwnelioeiqx5x2ez5yrgrzk7ypi",
+        "tokenID": tokenID,
     })
 }
 export default handler;
