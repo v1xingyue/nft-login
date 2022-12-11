e493be2ef4a2792f3693d4b6cb37fb1e83b56097
v1xingyue <qixingyue@126.com>
2022-12-12 00:04:55 +0800

    721 api test

---
+++ pages/api/meta721/[tokenID].js
@@ -0,0 +1,11 @@
+const handler = async (req, res) => {
+    const { tokenID } = req.query
+    console.log(tokenID)
+    res.json({
+        'name': 'OpenSea Creature Accessories',
+        'description': "Fun and useful accessories for your OpenSea creatures.",
+        'image': 'https://miro.medium.com/max/446/0*jr7S0JF8XiousKKz.png',
+        'external_link': 'https://github.com/ProjectOpenSea/opensea-erc1155/'
+    })
+}
+export default handler;
