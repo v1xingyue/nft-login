f044f53775b15ae3d6de30a1821860bba512e88d
v1xingyue <qixingyue@126.com>
2022-12-12 00:12:37 +0800

    add attributes

--- pages/api/meta721/[tokenID].js
+++ pages/api/meta721/[tokenID].js
@@ -5,7 +5,13 @@ const handler = async (req, res) => {
         'name': 'OpenSea Creature Accessories',
         'description': "Fun and useful accessories for your OpenSea creatures.",
         'image': 'https://miro.medium.com/max/446/0*jr7S0JF8XiousKKz.png',
-        'external_link': 'https://github.com/ProjectOpenSea/opensea-erc1155/'
+        'external_link': 'https://github.com/ProjectOpenSea/opensea-erc1155/',
+        'attributes': [
+            {
+                'trait_type': "level",
+                "value": "0"
+            }
+        ]
     })
 }
 export default handler;
